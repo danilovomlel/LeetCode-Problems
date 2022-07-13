1971011d355e1f36561a01c10805001cdf1ef8d1
Danilo Vomlel <108736504+danilovomlel@users.noreply.github.com>
2022-07-13 15:34:52 -0300

    Time: 1544 ms (5.00%), Space: 17.5 MB (28.03%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -7,14 +7,55 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        Nodes = []
+        #Otimo em tempo gasta mais memoria
+        #Nodes = []
+        #if not(head):
+        #    return head
+        #
+        #while(head.next):
+        #    Nodes.append(head)
+        #    if head.next in Nodes:
+        #        return head.next
+        #    head = head.next
+        #return head.next
+        
+        #Otima em memoria gasta mais tempo
         if not(head):
             return head
+        elif not(head.next):
+            return head.next
         
-        while(head.next):
-            Nodes.append(head)
-            if head.next in Nodes:
-                return head.next
-            head = head.next
-        return head.next
+        slow = head
+        faster = head.next
+        count_cycle = 0
+        
+        while(slow!=faster and faster.next):
+            slow = slow.next
+            if faster.next.next:
+                faster = faster.next.next
+            else:
+                return faster.next.next
             
+            if slow==faster:
+                faster = faster.next
+                count_cycle = 1
+                while(slow!=faster):
+                    slow = slow.next
+                    faster = faster.next.next
+                    count_cycle += 1
+        
+        if not(faster.next):
+            return faster.next
+                    
+        #count_cycle contem o numero de Node no ciclo
+        while(head!=slow):
+            for i in range(count_cycle):
+                slow = slow.next
+                if slow==head:
+                    return head
+            head = head.next
+            
+        return head
+        
+        
+        
