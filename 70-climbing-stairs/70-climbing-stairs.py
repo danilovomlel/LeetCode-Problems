548a17d6e70f11057b15319fafaae64eb34709a5
Danilo Vomlel <108736504+danilovomlel@users.noreply.github.com>
2022-07-26 12:03:38 -0300

    Time: 22 ms (99.56%), Space: 13.8 MB (96.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,32 +1,40 @@
 class Solution:
     
-    def fact(self, n: int, div=0) -> int:
-        if n==div:
-            return 1
-        else:
-            return n*self.fact(n-1, div)
+    #def fact(self, n: int, div=0) -> int:
+    #    if n==div:
+    #        return 1
+    #    else:
+    #        return n*self.fact(n-1, div)
     
-    def combinacao(self, n: int, e: int) -> int:
-        e_comp = n-e
+    #def combinacao(self, n: int, e: int) -> int:
+    #    e_comp = n-e
         
-        if e_comp > e: 
-            aux = e
-            e = e_comp
-            e_comp = aux
+    #    if e_comp > e: 
+    #        aux = e
+    #        e = e_comp
+    #        e_comp = aux
             
-        combin = int( self.fact(n,div=e)/self.fact(e_comp) )
-        return combin
+    #    combin = int( self.fact(n,div=e)/self.fact(e_comp) )
+    #    return combin
         
     def climbStairs(self, n: int) -> int:
         
-        soma = 0
-        escolhe = 0
+        fib0 = 0
+        fib1 = 1
+        n += 1
         
-        while(n>=escolhe):
-            soma += self.combinacao(n, escolhe)
-            n-=1
-            escolhe+=1
+        while(n>1):
+            n -= 1
+            fib0, fib1 = fib1, fib1+fib0
+        return fib1
         
-        return soma
+        #soma = 0
+        #escolhe = 0
         
+        #while(n>=escolhe):
+        #    soma += self.combinacao(n, escolhe)
+        #    n-=1
+        #    escolhe+=1
+        
+        #return soma
         
