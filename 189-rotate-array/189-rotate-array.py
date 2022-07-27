f25b7d6ff63946b82825a7df0bd760eaf2c00cf9
Danilo Vomlel <108736504+danilovomlel@users.noreply.github.com>
2022-07-27 18:43:15 -0300

    Time: 268 ms (77.25%), Space: 25.5 MB (27.59%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        sz = len(nums)
-        if k>sz:
-            k = k%sz
-        for i in range(k):
-            num = nums.pop()
-            nums.insert(0,num)
-        
         #sz = len(nums)
         #if k>sz:
         #    k = k%sz
-        #nums[:] = nums[sz-k:] + nums[:sz-k]
+        #for i in range(k):
+        #    num = nums.pop()
+        #    nums.insert(0,num)
+        
+        sz = len(nums)
+        if k>sz:
+            k = k%sz
+        nums[:] = nums[sz-k:] + nums[:sz-k]
